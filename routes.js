e9804a7ffe2ec9d18be3290c9a7966198cb845bd
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-21 11:14:30 -0300

    Organização de códigos.

--- routes.js
+++ routes.js
@@ -2,8 +2,12 @@ const Router = require('express').Router();
 const request = require('request');
 
 Router.get('/app2', function(req, res) {
+  // Realiza requisição em API externa.
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
-  res.send(body);
+  // Converte resultado de requisição em JSON.
+  body = JSON.parse(body);
+  // Exibe resultado.
+  res.json(body);
     });
 });
 
