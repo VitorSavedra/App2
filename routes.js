5e5b8787aa681a949818278045387f67a3426074
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-21 00:52:58 -0300

    Melhorias, correções, rotas e +

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 const Router = require('express').Router();
 const request = require('request');
 
-Router.get('/', function(req, res) {
+Router.get('/app2', function(req, res) {
   request('http://private-bc4b79-automobidummy.apiary-mock.com/users', function (error, response, body) {
   res.send(body);
     });
